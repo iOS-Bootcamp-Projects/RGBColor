7011adb7c9c9d710072d746bac5dc9d535a039f0
aamer Essa <aamer.es12@gmail.com>
2022-12-19 23:06:07 +0300

    V1.0

--- RGBColor/ContentView.swift
+++ RGBColor/ContentView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redValue: Double = 0
+    @State private var greenValue: Double = 0
+    @State private var blueValue: Double = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+            redView
+            greenView
+            blueView
+                  
+            Text("Result:")
+                Rectangle()
+                    .fill(Color(red: redValue, green: greenValue, blue: blueValue))
+                    .frame(width: 160, height: 160)
+                    .cornerRadius(20)
+            
+        }.padding()
     }
+    
+    var redView: some View {
+        VStack{
+            
+            Text("Red:")
+            Slider(value: $redValue,in: 0...1)
+                .tint(Color.red)
+            
+        }.padding()
+    } // end redView
+    
+    var greenView: some View{
+        VStack{
+            
+            Text("Green:")
+            Slider(value: $greenValue,in: 0...1)
+                    .tint(Color.green)
+            
+        }.padding()
+    }// end greenView
+    
+    var blueView: some View{
+        VStack{
+            
+            Text("Blue:")
+            Slider(value: $blueValue,in: 0...1)
+                .tint(Color.blue)
+            
+        }.padding()
+    }// end blueView
 }
 
 struct ContentView_Previews: PreviewProvider {
