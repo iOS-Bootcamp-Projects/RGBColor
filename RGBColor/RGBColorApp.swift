0e94b35bc7a0a782e00f1dedfcbf81ac096f8f55
aamer Essa <aamer.es12@gmail.com>
2022-12-19 22:26:20 +0300

    Initial Commit

---
+++ RGBColor/RGBColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBColorApp.swift
+//  RGBColor
+//
+//  Created by Aamer Essa on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
